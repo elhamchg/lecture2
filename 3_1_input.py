f11d299f3e6c48d2a8751b6be4bcd26caaa6e0e6
elhamchg <36910216+elhamchg@users.noreply.github.com>
2018-04-04 14:47:43 +0430

    Update 3_1_input.py
    به تعداد عدد گرفته شده از ورودی نام را چاپ کند

--- 3_1_input.py
+++ 3_1_input.py
@@ -1,11 +1,16 @@
 ##########################
 # get a string from user #
 ##########################
-text = input("enter what you want\n")
-print(5*text)
+#text = input("enter what you want\n")
+#print(5*text)
 
 ##########################
 # get a number from user #
 ##########################
-number=int(input("enter an integer\n"))
-print(5*number)
+#number=int(input("enter an integer\n"))
+#print(5*number)
+
+########################################
+name = input("نام خود را وارد کنید ")
+num = int(input("تعداد تکرار نام خود را وارد کنید"))
+print(num*name)
