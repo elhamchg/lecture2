7a60cf5d420e605e8c0924f8eb0facf4fe4ec392
elhamchg <36910216+elhamchg@users.noreply.github.com>
2018-04-04 12:14:04 +0430

    Update 1_2_example_variables.py
    محیط مربع به ضلع a

--- 1_2_example_variables.py
+++ 1_2_example_variables.py
@@ -3,16 +3,25 @@
 #####################################
 ### define a new varibale called pi #
 #####################################
-pi = 3.1415
+#pi = 3.1415
 #########################################
 ### define a new varibale called radius #
 #########################################
-radius = 2.2
+#radius = 2.2
 ########################################################
 ### define area and save the value of expression to it #
 ########################################################
-area = pi * (radius ** 2)
+#area = pi * (radius ** 2)
 ############################################
 ## print the calculated area of the circle #
 ############################################
+#print(area)
+
+#################
+#variable a The side is square
+#area = a * 4
+#####################
+a= 12
+area = a * 4
 print(area)
+
